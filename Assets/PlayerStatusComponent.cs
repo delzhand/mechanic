8bf52f771e48d5bc8d94a4fe338d1e8a035faa7e
Derek Adams <delzhand@gmail.com>
2018-04-29 20:14:21 -0400

    working toward melee attacks. added a demo enemy.

---
+++ Assets/PlayerStatusComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatusComponent : MonoBehaviour {
+
+    public int Health;
+    public int MaxHealth;
+
+    private void Awake()
+    {
+        MaxHealth = 5;
+        Health = 5;
+    }
+
+    private void Update()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Health: ");
+        for(int i = 0; i < MaxHealth; i++)
+        {
+            sb.Append(i < Health ? "O" : "_");
+            if (i < MaxHealth -1)
+            {
+                sb.Append(" ");
+            }
+        }
+        sb.Append("\n");
+        sb.AppendLine("Subitem A: None");
+        sb.AppendLine("Subitem B: None");
+        GetComponent<Text>().text = sb.ToString();
+    }
+}
---
+++ Assets/Scripts/LeaperEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaperEnemy : PhysicsObject
+{
+    private float timer = 0;
+    public bool facingRight = true;
+    [Range(0, 20)] public float jumpPowerH = 5;
+    [Range(0, 20)] public float jumpPowerV = 5;
+
+    protected override void ComputeVelocity() { 
+        if (grounded)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (grounded && timer >= 3)
+        {
+            timer -= 3;
+            velocity.y = jumpPowerV;
+            grounded = false;
+            facingRight = !facingRight;
+        }
+
+        if (!grounded)
+        {
+            targetVelocity.x = (facingRight ? jumpPowerH : -jumpPowerH);
+        }
+	}
+
+}
